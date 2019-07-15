89bf21626ceed0a3f6ffba9d913868de93ff6464
Allan Q <bolunqi@hotmail.com>
2019-07-14 17:37:53 -0700

    home page layout added

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,8 +1,8 @@
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="{{asset('js/jquery.js')}}js/"></script>
+<script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/fullpage.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/fullpage.extensions.min.js')}}"></script>
 
