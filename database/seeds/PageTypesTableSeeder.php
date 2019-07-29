f0a42d1192e359139159bc7d26f7425dfa11e6cb
Jason Li <jklsh0371@gmail.com>
2019-07-29 15:11:33 -0700

    jason

--- database/seeds/PageTypesTableSeeder.php
+++ database/seeds/PageTypesTableSeeder.php
@@ -2,30 +2,30 @@
 
 use Illuminate\Database\Seeder;
 
-class PageTypesTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
+// class PageTypesTableSeeder extends Seeder
+// {
+//     /**
+//      * Run the database seeds.
+//      *
+//      * @return void
+//      */
+//     public function run()
+//     {
 
-        DB::table('page_types')->insert([
-	        [
-	            'name' => 'home'
-	        ],
-	        [
-	            'name' => 'aboutus'
-	        ],
-	        [
-	            'name' => 'projects'
-	        ],
-	        [
-	            'name' => 'public_info'
-	        ]
-	    ]);
-	}
+//         DB::table('page_types')->insert([
+// 	        [
+// 	            'name' => 'home'
+// 	        ],
+// 	        [
+// 	            'name' => 'aboutus'
+// 	        ],
+// 	        [
+// 	            'name' => 'projects'
+// 	        ],
+// 	        [
+// 	            'name' => 'public_info'
+// 	        ]
+// 	    ]);
+// 	}
        
-}
+// }
